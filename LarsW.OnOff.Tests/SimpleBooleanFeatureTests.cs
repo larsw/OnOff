e181d01750f0423e58a173c527bafba40061efff
Lars Wilhelmsen <lars@sral.org>
2013-12-18 08:41:12 +0100

    changed the name of the feature set in test project / app.config.

--- LarsW.OnOff.Tests/SimpleBooleanFeatureTests.cs
+++ LarsW.OnOff.Tests/SimpleBooleanFeatureTests.cs
@@ -4,17 +4,19 @@
 
     public class SimpleBooleanFeatureTests
     {
+        private const string FeatureSetName = "featureSet1";
+
         [FactAttribute]
         public void Feature_Foo_is_Enabled()
         {
-            var manager = ConfigurationFileFeatureManager.Create("simpleFeatures");
+            var manager = ConfigurationFileFeatureManager.Create(FeatureSetName);
             Assert.True(manager.Features.Foo);
         }
 
         [FactAttribute]
         public void Feature_Bar_is_Disabled()
         {
-            var manager = ConfigurationFileFeatureManager.Create("simpleFeatures");
+            var manager = ConfigurationFileFeatureManager.Create(FeatureSetName);
             Assert.False(manager.Features.Bar);
         }
     }
